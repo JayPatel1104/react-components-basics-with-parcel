4ae09f134f12ca4e9d9163d707a9de0d08361bb3
Jay Patel <jupatel1104@gmail.com>
2024-01-27 20:59:09 -0500

    first functional component and noted important notes

--- App.js
+++ App.js
@@ -1,23 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-
-const heading = React.createElement("h1", { id: "heading" }, "namaste react");
-
-console.log(heading);
-
-/**
- * how jsx code is executed:
- *
- * Step 1: Babel converts the jsx code into reactDOM element
- * Step 2: now the reactDOM is converted into an JS object
- * Step 3: finally, the element is rendered into html file
- *
- * Note: This whole process is managed by parcel bundler
- *
- */
-
 const jsxHeading = (
   <h1 className="heading" tabIndex="5">
     Namaste React using JSX{" "}
@@ -27,5 +10,11 @@ const jsxHeading = (
 console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const HeadingComponent = () => (
+  <div id="container">
+    <h1>This is a heading component using functional component</h1>
+  </div>
+);
 
 root.render(jsxHeading);
