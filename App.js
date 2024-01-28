bb518f20be8cafa8bf1a718920ce20813daf8d8b
Jay Patel <jupatel1104@gmail.com>
2024-01-27 20:35:58 -0500

    learned to use jsx instead of React.createElement

--- App.js
+++ App.js
@@ -7,12 +7,25 @@ const heading = React.createElement("h1", { id: "heading" }, "namaste react");
 
 console.log(heading);
 
-//JSX
+/**
+ * how jsx code is executed:
+ *
+ * Step 1: Babel converts the jsx code into reactDOM element
+ * Step 2: now the reactDOM is converted into an JS object
+ * Step 3: finally, the element is rendered into html file
+ *
+ * Note: This whole process is managed by parcel bundler
+ *
+ */
 
-const jsxHeading = <h1>Namaste React using JSX</h1>;
+const jsxHeading = (
+  <h1 className="heading" tabIndex="5">
+    Namaste React using JSX{" "}
+  </h1>
+);
 
 console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(jsxHeading);
