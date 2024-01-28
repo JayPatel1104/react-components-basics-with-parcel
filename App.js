ddca7715b875924bf61936627bd47aa5df9eb345
Jay Patel <jupatel1104@gmail.com>
2024-01-27 23:13:37 -0500

    accomplished components

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = (
+const title = (
   <h1 className="heading" tabIndex="5">
     Namaste React using JSX{" "}
   </h1>
 );
 
-console.log(jsxHeading);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
+const number = 1000;
 const HeadingComponent = () => (
   <div id="container" className="heading">
+    {title}
     <h1>This is a heading component using functional component</h1>
   </div>
 );
